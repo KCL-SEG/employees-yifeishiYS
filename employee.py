51c99cfe45c18bfe618534eafdece896573c522f
Kiyoaki <yifei.shi@kcl.ac.uk>
2023-09-27 16:45:00 +0100

    Update employee.py for test

--- employee.py
+++ employee.py
@@ -4,22 +4,36 @@
 class Employee:
     def __init__(self, name):
         self.name = name
+        self.total_pay = 0
 
     def get_pay(self):
-        pass
+        return self.total_pay
 
     def __str__(self):
-        return self.name
+        employee_record = "{} works on a ".format(self.name)
+        return employee_record
 
+    def set_monthly_salary(self, salary):
+        self.total_pay += salary
+
+    def set_hourly_salary(self, time, salary):
+        self.total_pay += time * salary
+
+    def set_commission(self, num_contract, bonus):
+        self.total_pay += num_contract * bonus
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
 billie = Employee('Billie')
+billie.set_monthly_salary(4000)
 
 # Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
 charlie = Employee('Charlie')
+charlie.set_hourly_salary(100, 25)
 
 # Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
 renee = Employee('Renee')
+renee.set_monthly_salary(3000)
+renee.set_commission(4, 200)
 
 # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
 jan = Employee('Jan')
